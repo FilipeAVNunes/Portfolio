cd7028f7e30cec67fea566d523d5d3d393dd4d7d
FilipeAVNunes <filipenunes407@hotmail.com>
2020-09-22 17:56:54 +0100

    tech stack half way through

--- src/components/Technologies/technologies.component.jsx
+++ src/components/Technologies/technologies.component.jsx
@@ -1,36 +1,50 @@
 import React from "react";
-import "./technologies.styles.css";
+import "./technologies.styles.scss";
 
 function Technologies() {
   return (
     <div className="tech">
-      <h1>Some Technologies I've worked with</h1>
-      <ul>
-        <li>React.js</li>
-        <img className="logo" src="images/ReactJS.png" alt="react icon" />
-        <li>HTML</li>
-        <img className="logo" src="images/HTML5.png" alt="html icon" />
-        <li>CSS</li>
-        <img className="logo" src="images/CSS3.png" alt="css icon" />
-        <li>Javascript</li>
-        <img
-          className="logo"
-          src="images/javascript.jpg"
-          alt="javascript icon"
-        />
-        <li>Node.js</li>
-        <img className="logo" src="images/nodeJS.jpg" alt="nodejs icon" />
-        <li>Express.js</li>
-        <img
-          className="logo"
-          src="images/node-express.png"
-          alt="expressjs icon"
-        />
-        <li>MongoDB</li>
-        <img className="logo" src="images/mongoDB.png" alt="mongodb icon" />
-        <li>Bootstrap</li>
-        <img className="logo" src="images/bootstrap.jpg" alt="bootstrap icon" />
-      </ul>
+      <h1>Tech Stack</h1>
+      <div className="tech-container">
+        <div className="tech-column">
+          <h4>FRONT END</h4>
+          <ul>
+            <li>React.Js</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
+        <div className="tech-column">
+          <h4>BACK END</h4>
+          <ul>
+            <li>Node.Js</li>
+            <li>Express.js</li>
+            <li>MongoDB</li>
+            <li>RestAPI</li>
+          </ul>
+        </div>
+        <div className="tech-column">
+          <h4>TOOLS</h4>
+          <ul>
+            <li>NPM</li>
+            <li>Git</li>
+            <li>Github</li>
+            <li>Postman</li>
+          </ul>
+        </div>
+      </div>
+
+      {/*
+      <p>React.js</p>
+      <p>HTML</p>
+      <p>CSS</p>
+      <p>Javascript</p>
+      <p>Node.js</p>
+      <p>Express.js</p>
+      <p>MongoDB</p>
+      <p>Bootstrap</p>*/}
     </div>
   );
 }
