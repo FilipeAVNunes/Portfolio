524b7b3377fbb6f57db7446a21a31790e2db1e51
FilipeAVNunes <filipenunes407@hotmail.com>
2020-10-13 14:34:50 +0100

    edited e-commerce description

--- src/components/ProjectsCarousel/projects-carousel.component.jsx
+++ src/components/ProjectsCarousel/projects-carousel.component.jsx
@@ -28,7 +28,7 @@ const ProjectsCarousel = () => {
             >
               Check out more!
             </Button>
-            <p>Simple E-commerce website with React.js.</p>
+            <p>Still in progress E-commerce website with React.js.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
